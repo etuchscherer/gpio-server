938716365089c5891cc65928e1c4c4a6fea128d4
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2020-06-04 13:42:35 -0700

    Adding the test controller for rest / led capability.

--- source/router.js
+++ source/router.js
@@ -1,9 +1,12 @@
-import healthcheck from '@/lib/routes/healthcheck';
 import express from 'express';
-
+import healthcheck from '@/lib/routes/healthcheck';
+import testController from '@/lib/controllers/test-controller';
 
 const router = express.Router();
 
 router.get('/healthcheck', healthcheck);
+
+router.post('/test-led/:pin', testController.ledOn);
+router.get('/test-led/:pin', testController.ledOff);
 
 export default router;
