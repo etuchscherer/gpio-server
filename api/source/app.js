c03886effca81082156a81644718e5840ba381da
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-05 14:39:57 -0700

    Moving cron-scheduler to middleware

--- api/source/app.js
+++ api/source/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import router from '@/router';
 import gpioService from '@/services/gpio';
-import LED from '@/objects/led';
-import cron from 'node-cron';
+import cronScheduler from '@/middleware/cron-scheduler';
 
 const app = express();
 const port = 3000;
@@ -11,24 +10,7 @@ console.log('initializing gpio service…');
 app.gpioService = gpioService;
 console.log('done!');
 
-console.log('initializing main feed pump…');
-// TODO:: do not hardcode
-// app.gpioService.set(18, new LED(18));
-
-// Run every hour at 0 minutes
-cron.schedule('0 * * * *', () => {
-  console.log('turning main feed pump on');
-  // app.gpioService.get(18).on();
-});
-
-// run on minute ten of every hour
-cron.schedule('10 * * * *', () => {
-  console.log('turning main feed pump off');
-  // app.gpioService.get(18).off();
-});
-
-console.log('done!');
-
+app.use(cronScheduler);
 app.use(router);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ api/source/middleware/cron-scheduler.js
@@ -0,0 +1,24 @@
+import cron from 'node-cron';
+
+export default cronScheduler = function(req, res, next) {
+
+  console.log('initializing main feed pump…');
+  // TODO:: do not hardcode
+  req.app.gpioService.set(18, new LED(18));
+
+  // Run every hour at 0 minutes
+  cron.schedule('0 * * * *', () => {
+    console.log('turning main feed pump on');
+    req.app.gpioService.get(18).on();
+  });
+
+  // run on minute ten of every hour
+  cron.schedule('10 * * * *', () => {
+    console.log('turning main feed pump off');
+    req.app.gpioService.get(18).off();
+  });
+
+  console.log('done!');
+
+  return next();
+};
