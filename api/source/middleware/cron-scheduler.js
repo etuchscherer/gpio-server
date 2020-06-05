0f4c05265e2be2249a7f8b09f766c9c91e7c14c0
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-05 14:42:10 -0700

    CronScheduler now using Pin, instead of LED.

--- api/source/middleware/cron-scheduler.js
+++ api/source/middleware/cron-scheduler.js
@@ -1,10 +1,11 @@
+import Pin from '@/objects/pin';
 import cron from 'node-cron';
 
 export default cronScheduler = function(req, res, next) {
 
   console.log('initializing main feed pump…');
   // TODO:: do not hardcode
-  req.app.gpioService.set(18, new LED(18));
+  req.app.gpioService.set(18, new Pin(18));
 
   // Run every hour at 0 minutes
   cron.schedule('0 * * * *', () => {
