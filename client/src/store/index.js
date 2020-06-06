664e760b0a977fee2fbbbc27747457e806c10fe1
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-05 22:40:52 -0700

    Adding client, still have some cleanup to do

---
+++ client/src/store/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    components: {
+      pump: {
+        isEnergized: false,
+        enabled: true
+      },
+      light: {
+        isEnergized: false,
+        enabled: false
+      },
+      fan: {
+        isEnergized: false,
+        enabled: false
+      }
+    }
+  },
+  getters: {
+    pump(state) {
+      return state.components.pump;
+    }
+  },
+  mutations: {
+    setPump(state, isEnergized) {
+      state.components.pump.isEnergized = isEnergized;
+    }
+  },
+  actions: {
+    togglePump({ commit }) {
+      fetch("http://localhost:3000/healthcheck")
+        .then(r => r.json())
+        .then(data => {
+          console.log(data);
+          // TODO:: use data.isOn
+          commit("setPump", data.success);
+        });
+    }
+  },
+  modules: {}
+});
