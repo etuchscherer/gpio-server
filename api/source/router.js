90023ed94401eab56eaabb0c1e476c3ef219b7e9
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-06 23:58:56 -0700

    Removing quite a bit of cruft

--- api/source/router.js
+++ api/source/router.js
@@ -3,7 +3,6 @@ import healthcheck from '@/controllers/healthcheck';
 import { read, set, destroy, status } from '@/controllers/pins';
 import { toggle as togglePump } from '@/controllers/pumps';
 import { toggle as toggleLight } from '@/controllers/lights';
-import Relay from '@/objects/relay';
 
 const router = express.Router();
 
@@ -11,16 +10,6 @@ const router = express.Router();
 router.get('/healthcheck', healthcheck);
 
 // experimental routes
-router.get('/test', function(req, res) {
-  const relay = new Relay('main lighting', 17);
-  req.app.gpioService.set(17, relay);
-  res.send('done');
-});
-
-router.post('/test', function(req, res) {
-  const isEnergized = req.app.gpioService.get(17).toggle().isEnergized();
-  res.json({ isEnergized });
-});
 
 // functional routes
 router.get('/pins', status);
