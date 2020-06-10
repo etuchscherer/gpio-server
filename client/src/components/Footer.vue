72a92a2b35b82a9a21dc0379e5d69a60f0fa443c
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-09 22:19:38 -0700

    Adding Footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="flex items-stretch bg-gray-200 h-18">
+      <div class="flex-1 pa-2 m-2"></div>
+      <router-link
+        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 pa-2 m-2"
+        to="/"
+      >
+        <p>Main</p>
+      </router-link>
+
+      <router-link
+        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 pa-2 m-2"
+        to="/controls"
+      >
+        Overrides
+      </router-link>
+    </div>
+  </footer>
+</template>
