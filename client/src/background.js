60f8328436bfddf032043c17ebbd1c4b13db4e89
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-10 23:41:05 -0700

    Making electron go fullscreen

--- client/src/background.js
+++ client/src/background.js
@@ -19,8 +19,9 @@ protocol.registerSchemesAsPrivileged([
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    show:false,
+    // width: 800,
+    // height: 600,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
@@ -31,12 +32,15 @@ function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    if (process.env.OS_ENV !== 'pi') win.webContents.openDevTools();
   } else {
     createProtocol("app");
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
+
+  win.maximize();
+  win.show();
 
   win.on("closed", () => {
     win = null;
