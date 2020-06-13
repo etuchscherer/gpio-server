6dd61da48cf3942327e4f6108851be6947dab1c2
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-13 01:51:44 -0700

    Scripts to run in kiosk mode.

--- api/source/controllers/healthcheck.js
+++ api/source/controllers/healthcheck.js
@@ -3,7 +3,7 @@ import { timestamp as ts } from '@/utils/timestamp';
 export default (req, res) => {
   const message = 'this is a message';
   const level = 'info';
-  req.logger.log({ level, message });
+  // req.logger.log({ level, message });
   const timestamp = ts();
   res.json({ success: true, timestamp });
 };
