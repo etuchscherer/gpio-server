185905d23b20f756bd87469114d89e4d8e1bf997
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-13 02:27:54 -0700

    making the footer butttons bigger

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,21 +2,21 @@
   <footer>
     <div class="flex items-stretch bg-gray-200 h-18">
       <router-link
-        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 pa-2 m-2"
+        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 h-32 p-8 m-2"
         to="/logs"
       >
         <p>Logs</p>
       </router-link>
 
       <router-link
-        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 pa-2 m-2"
+        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 h-32 p-8 m-2"
         to="/"
       >
         <p>Main</p>
       </router-link>
 
       <router-link
-        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 pa-2 m-2"
+        class="flex-1 text-2xl text-gray-700 text-center bg-gray-400 h-32 p-8 m-2"
         to="/controls"
       >
         Overrides
