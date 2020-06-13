6e40ed28cdded7bc6077f6de123e9d264ed46d3f
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-13 13:32:03 -0700

    Adding 'fan' to init hardware

--- api/source/tasks/init-hardware.js
+++ api/source/tasks/init-hardware.js
@@ -1,11 +1,13 @@
 const PUMP = 18;
 const LIGHT = 17;
+const FAN = 27;
 
 const init = function(app) {
   const { gpioService } = app;
   // TODO:: do not hardcode
   gpioService.findOrCreateRelay(PUMP, 'pump');
   gpioService.findOrCreateRelay(LIGHT, 'light');
+  gpioService.findOrCreateRelay(FAN, 'fan');
 };
 
 export default init;
