ffc9831b18b0e9f7a3b056aedf8d1349190991e9
Eric Tuchscherer <eric@tuchscherer.me>
2020-06-24 05:47:58 -0700

    Stashing for test on dev instance

--- api/source/objects/temp-sensor.js
+++ api/source/objects/temp-sensor.js
@@ -1,13 +1,17 @@
 import { getTempC } from '@/tasks/temp-sensor';
-import { celsiusToFahrenheit } from '@/utils/celsius-to-fahrenheit';
-import moment from 'moment';
+// import { celsiusToFahrenheit } from '@/utils/celsius-to-fahrenheit';
+// import moment from 'moment';
 
 export default class Temp {
 
   constructor(id = 4) {
+    this.subscribers = [];
     this.name = 'temp sensor';
     this.typeOf = '1 wire interface';
+    this.lastUpdated;
     this.id = id;
+    // this.addSubscriber('_toF', (temp) => this.tempF = celsiusToFahrenheit(temp.tempC));
+    // this.addSubscriber('_lastUpdated', () => this.lastUpdated = moment().format());
   }
 
   /**
@@ -34,11 +38,26 @@ export default class Temp {
   }
 
   /**
+   * Adds a subscriber function, that will fire whenever
+   * a temperature change is detected. The
+   * @param {string} key the name of the subscriber
+   * @param {function} callback callback arg is this temp-sensor object
+   */
+  addSubscriber(key, callback) {
+    this.subscribers.push({ key, callback });
+  }
+
+  /**
    * reads from the sensor and sets to memory;
    */
   _setTemp() {
     this.tempC = getTempC();
-    this.tempF = celsiusToFahrenheit(this.tempC);
-    this.lastUpdated = moment().format();
+    this._doSubscribers();
+    // this.tempF = celsiusToFahrenheit(this.tempC);
+    // this.lastUpdated = moment().format();
+  }
+
+  _doSubscribers() {
+    this.subscribers.forEach(el => el.callback);
   }
 }
